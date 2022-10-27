899235ea48f731f11b0ef19b1c1211b0a42fffdc
Julian Salcedo Torres <jsalcedo218@gmail.com>
2022-10-26 21:23:25 -0500

    Update SharkPontoon.php

--- codewars/8 kyu/SharkPontoon.php
+++ codewars/8 kyu/SharkPontoon.php
@@ -1,3 +1,5 @@
+<!-- https://www.codewars.com/kata/57e921d8b36340f1fd000059 -->
+
 <?php
 
 function shark(int $pontoonDistance, int $sharkDistance, int $youSpeed, int $sharkSpeed, bool $dolphin): string
