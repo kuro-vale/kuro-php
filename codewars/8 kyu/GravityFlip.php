2525a2f3ee15d7136911a5e4caada173a9b3ca80
Julian Salcedo Torres <jsalcedo218@gmail.com>
2022-10-29 13:32:24 -0500

    Create GravityFlip.php

---
+++ codewars/8 kyu/GravityFlip.php
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5f70c883e10f9e0001c89673/php
+
+function flip(string $dir, array $arr): array
+{
+    if ($dir == 'L') {
+        rsort($arr);
+    } elseif ($dir == 'R') {
+        sort($arr);
+    } else {
+        echo "Please enter a valid direction ('L' or 'R')";
+    }
+    return $arr;
+}
