7edf9df456668a32b5d285f3e149b2668173f9a6
Julian Salcedo Torres <jsalcedo218@gmail.com>
2022-11-01 12:21:20 -0500

    Create closest_elevator.php

---
+++ codewars/8 kyu/closest_elevator.php
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5c374b346a5d0f77af500a5a/php
+
+<?php
+
+function elevator($left, $right, $call): string
+{
+    $left_distance = abs($left - $call);
+    $right_distance = abs($right - $call);
+    if ($left_distance < $right_distance) {
+        return "left";
+    } else {
+        return "right";
+    }
+}
